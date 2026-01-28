b3b3983c0f0c01e1e22b08bf99cca229b72c5a83
G.M Erandi Hansika <erandi2287hansika@gmail.com>
2026-01-28 22:03:52 +0530

    implement main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   
+    <GoogleOAuthProvider clientId="643620464773-vt4d4pr5jnl1u16cl16v7dohfoq2m21k.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
